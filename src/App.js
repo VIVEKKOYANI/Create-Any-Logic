1bbad06a953a97237fcb534746dd2613ae3a1740
vivek <vivek.koyani@gmail.com>
2020-05-05 23:37:23 +0530

    complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
-
+import GateKeeper from './GateKeeper';
 function App() {
   return (
     <div>
-      <p>{new Date().toLocaleString()}  </p>
+      <p>{new Date().toLocaleString()}</p>
+      <GateKeeper />
     </div>
   );
 }
