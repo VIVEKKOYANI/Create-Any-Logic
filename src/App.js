532d3666dfd8bbee5d4858c192a8015b564e3808
vivek <vivek.koyani@gmail.com>
2020-05-05 23:12:38 +0530

    add setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <p>{new Date().toLocaleString()}  </p>
+    </div>
+  );
+}
+
+
+export default App;
