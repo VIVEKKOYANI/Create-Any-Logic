86e08a37b86091a1971061e7f22dcb42beb3135f
vivek <vivek.koyani@gmail.com>
2020-05-07 09:56:45 +0530

    add component

---
+++ A Simple Calculator Application With React.JS/src/components/KeypadComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class KeypadComponent extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <>
+            </>
+         );
+    }
+}
+ 
+export default KeypadComponent;
---
+++ A Simple Calculator Application With React.JS/src/components/ResultComponent.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+
+class ResultComponent extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <>
+            </>
+         );
+    }
+}
+ 
+export default ResultComponent;
